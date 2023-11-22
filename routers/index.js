8fdaee61877ee25430af9f8d7d40c9660e7f8183
maldini19 <maldiniradjab@gmail.com>
2023-11-22 22:27:10 +0700

    update controller dan ejs

--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const Controller = require('../controllers/controller')
+const bolabio = require('./bolabio')
 
 router.get('/', Controller.home)
 
-
+router.use("/bolabio", bolabio)
 
 
 
