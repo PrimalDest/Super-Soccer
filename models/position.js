002a82cc419d3b1a9f03ce7da358fcb70867a409
maldini19 <maldiniradjab@gmail.com>
2023-11-22 19:32:40 +0700

    baru ngisi app

--- models/position.js
+++ models/position.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Position.belongsTo(models.BolaBio)
     }
   }
   Position.init({
---
+++ routers/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const Controller = require('../controllers/controller')
+
+router.get('/', Controller.home)
+
+
+
+
+
+module.exports = router
