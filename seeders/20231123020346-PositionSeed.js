708241334be1d4f7096ff9a9b69bd23c43bfe7e9
muhammad.mahdi <aljufrimuhammad5@gmail.com>
2023-11-23 10:01:05 +0700

    Update dan tambah seeder

---
+++ seeders/20231123020346-PositionSeed.js
@@ -0,0 +1,35 @@
+'use strict';
+const fs = require('fs')
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    let Position = fs.readFileSync('./data/position.json')
+    Position = JSON.parse(Position)
+    Position = Position.map(position => {
+      delete position.id
+      position.createdAt = new Date();
+      position.updatedAt = new Date();
+      return position;
+    });
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Positions', Position)
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Positions', null, {})
+  }
+};
