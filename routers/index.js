70f5c6905b3dea86812282876525d2a633efd4eb
muhammad.mahdi <aljufrimuhammad5@gmail.com>
2023-11-23 11:39:55 +0700

    Update controller, router dan view

--- routers/index.js
+++ routers/index.js
@@ -1,13 +1,34 @@
-const express = require('express')
-const router = express.Router()
-const Controller = require('../controllers/controller')
-const bolabio = require('./bolabio')
-const login = require('./login')
+const express = require('express');
+const router = express.Router();
+const Controller = require('../controllers/controller');
 
-router.get('/', Controller.login)
+router.get('/', Controller.home); // halaman rumah
 
-router.use("/bolabio", bolabio)
-router.use("/login", login)
+router.get('/home1', Controller.home1); // halaman rumah
+
+router.get('/register', Controller.register); // register page => hanya untuk user
+
+router.post('/register', Controller.register);
+
+router.get('/login', Controller.login); // login page => user dan admin
+
+router.post('/login', Controller.login);
+
+router.get('/logout', Controller.logout); // logout route
+
+router.get("/bio", Controller.showAllBio)
+
+router.get("/add", Controller.addBio)
+
+// router.get("/search", Controller.search)
+
+// router.get("/:id", Controller.detail)
+
+router.post("/add", Controller.postAddBio)
+
+router.post("/:id/edit", Controller.editBio)
+
+router.get("/:id/delete", Controller.delete)
 
 
-module.exports = router
+module.exports = router;
--- routers/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const login = express.Router()
-const controller = require('./controller')
-
-login.get('/login', controller.loginPage)
-// login.post('/login', controller.postLogin)
-
-module.exports = router
